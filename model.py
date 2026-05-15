dee2dc72e30e48c1907a1a15822946cb336ffe83
Divyaansh0711 <dkjha0711@gmail.com>
2026-05-15 16:50:41 +0530

    changes made

--- model.py
+++ model.py
@@ -46,8 +46,8 @@ except ImportError:
 
 
 # Fill these after training and uploading files to Google Drive.
-CHECKPOINT_GDRIVE_ID = ""
-VOCAB_GDRIVE_ID = ""
+CHECKPOINT_GDRIVE_ID = "https://drive.google.com/file/d/1XCP8S-A6buXo0aWTC13nYumhATWYX_mA/view?usp=share_link"
+VOCAB_GDRIVE_ID = "https://drive.google.com/file/d/1Bi9f3PeapZX5M-eCtT6eFAc9k89BmiWt/view?usp=share_link"
 
 DEFAULT_CHECKPOINT_PATH = "checkpoint.pt"
 DEFAULT_VOCAB_PATH = "vocabs.json"
